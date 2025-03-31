e186b471f83c68c035fc3da487e8e80389cdc702
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-03-31 12:12:25 -0600

    view: main layout app created

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import BudgetForm from "./components/BudgetForm"
 
 
 function App() {
@@ -5,7 +6,13 @@ function App() {
 
   return (
     <>
-      <h1>Control Gastos</h1>
+      <header className="bg-blue-600 py-8 max-h-72">
+        <h1 className="uppercase text-4xl text-white font-black  text-center">Control de Gastos</h1>
+      </header>
+
+      <div className="max-w-3xl mx-auto mt-10 p-10 rounded-lg shadow-lg bg-white">
+      <BudgetForm />
+      </div>
     </>
   )
 }

49e6d39997c7843594dccbe6c8e2d78ccf2726d7
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-04-01 13:38:07 -0600

    update: bugettracker, Modal elements added

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-import BudgetForm from "./components/BudgetForm"
-
+import { useMemo } from "react";
+import BudgetForm from "./components/BudgetForm";
+import { useBuget } from "./hooks/useBuget";
+import BudgetTracker from "./components/BudgetTracker";
+import ExpenseModal from "./components/ExpenseModal";
 
 function App() {
- 
+  const { state } = useBuget();
+
+  const isValidBuget = useMemo(() => state.budget > 0, [state.budget]);
 
   return (
     <>
       <header className="bg-blue-600 py-8 max-h-72">
-        <h1 className="uppercase text-4xl text-white font-black  text-center">Control de Gastos</h1>
+        <h1 className="uppercase text-4xl text-white font-black  text-center">
+          Control de Gastos
+        </h1>
       </header>
 
       <div className="max-w-3xl mx-auto mt-10 p-10 rounded-lg shadow-lg bg-white">
-      <BudgetForm />
+        {isValidBuget ? <BudgetTracker /> : <BudgetForm />}
       </div>
+
+      {isValidBuget && <ExpenseModal />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;

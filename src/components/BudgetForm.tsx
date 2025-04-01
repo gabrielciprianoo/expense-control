0d1905f5c7dd1eba44ae955c1d0eaeddebe2ff16
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-04-01 13:36:49 -0600

    refactor: changes in variables and use custom hook buget to set buget into global state

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,19 +1,29 @@
 import { useMemo, useState } from "react";
+import { useBuget } from "../hooks/useBuget";
+
 
 export default function BudgetForm() {
 
     const [budget, setBudget] = useState(0);
+    const { dispatch } = useBuget();
 
     const handleOnChange = ( e : React.ChangeEvent<HTMLInputElement> )=> {
         setBudget(e.target.valueAsNumber);
     }
 
-    const isValid = useMemo( () => {
+    const isInvalid = useMemo( () => {
         return isNaN(budget) || budget <= 0 || budget === null || budget === undefined
     }, [budget])
 
+    const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (isInvalid) return;
+        dispatch({ type: "SET_BUDGET", payload: {budget: budget} });
+        
+    }
+
   return (
-        <form className="space-y-5 ">
+        <form className="space-y-5 " onSubmit={handleOnSubmit}>
             <div className="flex flex-col space-y-5">
                 <label 
                 htmlFor="budget" 
@@ -28,7 +38,7 @@ export default function BudgetForm() {
                     id="budget"
                     className="w-full bg-white border border-gray-200 p-2"
                     placeholder="Define tu presupuesto"
-                    value={isValid ? "" : budget}
+                    value={isInvalid ? "" : budget}
                     onChange={handleOnChange}
                     min={0}
                 />  
@@ -38,7 +48,7 @@ export default function BudgetForm() {
                  type="submit"
                  value={"Agregar Presupuesto"}
                  className="bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-4 cursor-pointer transition-colors duration-300 uppercase disabled:opacity-50 disbled:cursor-not-allowed"
-                 disabled={isValid}
+                 disabled={isInvalid}
              />
         </form> 
 

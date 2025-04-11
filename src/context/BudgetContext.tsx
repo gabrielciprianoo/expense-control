c4352e88beb605eaa013885d94e1ad335509d631
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-04-11 15:39:28 -0600

    feature: exxport global amounts in the context

--- src/context/BudgetContext.tsx
+++ src/context/BudgetContext.tsx
@@ -1,10 +1,12 @@
 /* eslint-disable react-refresh/only-export-components */
-import { useReducer, createContext, Dispatch, ReactNode } from "react"
-import { BudgetActions, budgetReducer, BudgetState, initialState } from "../reducers/budgetReducer"
+import { useReducer, createContext, Dispatch, ReactNode, useMemo } from "react"
+import { BudgetActions, budgetReducer, BudgetState, initialState } from "../reducers/budgetReducer";
 
 type BudgetContextProps = {
     state: BudgetState
-    dispatch: Dispatch<BudgetActions>
+    dispatch: Dispatch<BudgetActions>,
+    totalExpenses: number
+    remainningBudget: number
 }
 
 type BudgetProviderProps = {
@@ -16,10 +18,20 @@ export const BudgetContext = createContext<BudgetContextProps>(null!)
 export const BudgetProvider = ( {children} : BudgetProviderProps ) =>{
 
     const [state, dispatch] = useReducer(budgetReducer, initialState)
+    
+
+    const totalExpenses = useMemo(()=> state.expenses.reduce((total , expense) => expense.amount + total,0 ), [state.expenses])
+
+    const remainningBudget = useMemo(() => state.budget - totalExpenses, [state.budget, totalExpenses])
    
     return (
         <BudgetContext.Provider 
-        value={{ state, dispatch }}
+            value={{
+                 state,
+                 dispatch,
+                 totalExpenses,
+                 remainningBudget
+            }}
         >
             {children}
         </BudgetContext.Provider>

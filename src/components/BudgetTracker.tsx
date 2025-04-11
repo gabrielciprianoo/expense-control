1559907527a58c1f55c045b8f85a825da5ba81da
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-04-11 16:05:33 -0600

    feature: dynamic progressbar

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,20 +1,34 @@
 import AmountDisplay from "./AmountDisplay";
 import { useBuget } from "../hooks/useBuget";
-
-
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 export default function BudgetTracker() {
     const { state, totalExpenses, remainningBudget } = useBuget();
+    const percentage = ((totalExpenses / state.budget) * 100).toFixed(2);
+    const progressBarColor = +percentage === 100 ? '#dc2626' : '#3b82f6';
   return (
     <div className="grid grid-cols-1  md:grid-cols-2 gap-5">
 
         <div className="flex justify-center">
-            <img src="/grafico.jpg" alt="Grafica de gastos" />
+            <CircularProgressbar
+                value={+percentage}
+                styles={buildStyles({
+                    pathColor: progressBarColor,
+                    trailColor: '#f5f5f5',
+                    textColor: progressBarColor,
+                    textSize: 8
+                })}
+                text={`${percentage}% Gastado`}
+            />
         </div>
 
         <div className="flex flex-col justify-center items-center gap-8">
             <button 
                 type="button"
                 className=" bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg cursor-pointer hover:bg-pink-800 transition-colors duration-300"
+                onClick={() => {
+                    
+                }}
             >
                 Restablecer Presupuesto
             </button>
